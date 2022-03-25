7839b55d6b522023e721935617bd400c494ff41f
REX <cssmith431@yahoo.com>
2022-03-25 14:23:35 -0400

    x

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const routes = require('./routes');
+const path = require('path');
+//const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use(session(sess));
+
+app.use(require('./controllers/'));
 
 
-app.use(routes);
-
-
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
